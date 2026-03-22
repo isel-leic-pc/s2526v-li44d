504263a15ca0e6b821fd77d756e70940c5898a33
Paulo Pereira <paulo.pereira@isel.pt>
2026-03-22 14:01:23 +0000

    Adds demo code from last lecture

--- demos/Miscellaneous/src/main/kotlin/ValueHolder.kt
+++ demos/Miscellaneous/src/main/kotlin/ValueHolder.kt
@@ -1,34 +1,46 @@
 package palbp.demos.pc.isel
 
+import java.util.concurrent.CountDownLatch
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
 
-class ValueHolder(var value: Int? = null) {
+class ValueHolder(private var value: Int? = null) {
 
     private val guard = ReentrantLock()
+    private val condition = guard.newCondition()
 
     /**
      * Blocks the calling thread until the value is available.
      */
-    fun getValue(): Int = guard.withLock {
+    fun getValue(): Int {
+        guard.withLock {
 
-        val observedValue = value
-        if (observedValue != null) {
-            return observedValue
+            val observedValue: Int? = value
+            if (observedValue != null) {
+                return observedValue
+            }
+
+            while (true) {
+
+                condition.await()
+
+                val observedValue = value
+                if (observedValue != null) {
+                    return observedValue
+                }
+
+            }
         }
-
-        TODO("wait for value")
     }
 
     /**
      * Sets the value and notifies any waiting threads.
      */
     fun putValue(value: Int): Unit = guard.withLock {
-        if (this.value != null) {
-            throw IllegalStateException("Value already set")
-        }
+
+        check(this.value != null) { "Value already set" }
 
         this.value = value
-        TODO("notify all waiting threads")
+        condition.signalAll()
     }
 }
